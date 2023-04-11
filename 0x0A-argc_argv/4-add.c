2d50a2f1648bea224d6224a09c870e3caeac12bc
PhefuM <phefumadyo@gmail.com>
2023-04-10 19:09:35 -0700

    To infinity and beyond

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
+#include <ctype.h>
+#include  "main.h"
 /**
  * main - takes in all integer arguments and returns the sum
  * @argc: number of command line arguments
