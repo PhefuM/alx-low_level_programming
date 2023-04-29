4b2cf4053239b775f83b5c5d772055158e4ca3c9
PhefuM <phefumadyo@gmail.com>
2023-04-28 20:21:56 -0700

    update

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+void first(void) __attribute__ ((constructor));
+/**
+ * first - prints a sense before the
+ * main function is executed
+ */
+
+void first(void)
+{
+	printf("You're beat! and yet, you must also allow, \n");
+	printf("I bore my house upon my back!\n");
+}
