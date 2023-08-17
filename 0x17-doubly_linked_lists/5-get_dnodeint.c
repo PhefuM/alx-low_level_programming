bdf05e4aac684247df8c80933e1a3258eba2f771
PhefuM <phefumadyo@gmail.com>
2023-08-17 13:13:33 -0700

    returns the nth node of a dlistint_t linked list

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - returns the 
+ * nth node of a dlisint_t linked list
+ *
+ * @head: head of the list 
+ * @index; index of the nth node
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	for (; index != 0; index--)
+	{
+		if (head == NULL)
+			return (NULL);
+		head = head->next;
+	}
+	return (head);
+}
