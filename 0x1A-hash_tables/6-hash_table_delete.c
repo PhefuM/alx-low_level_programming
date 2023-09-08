39357f37a6ccaf9b9633ef42d4aabcc52df9624b
PhefuM <phefumadyo@gmail.com>
2023-09-08 04:18:32 -0700

    >>> del ht

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -10,7 +10,7 @@ void hash_table_delete(hash_table_t *ht)
 {
 	unsigned long int i;
 	hash_node_t *temp1;
-	hash_node_t *tepm2;
+	hash_node_t *temp2;
 
 	if (ht == NULL)
 		return;
@@ -22,7 +22,7 @@ void hash_table_delete(hash_table_t *ht)
 			temp1 = temp1->next;
 			free(temp2->key);
 			free(temp2->value);
-			free(temp);
+			free(temp2);
 		}
 	}
 	free(ht->array);
